29212719cf983af56a8c36a9cf5eaf0ffad8be44
pal-6 <sarann93@gmail.com>
2019-04-19 15:23:06 +0530

    Read from file system

--- src/main/java/org/superbiz/moviefun/CsvUtils.java
+++ src/main/java/org/superbiz/moviefun/CsvUtils.java
@@ -6,6 +6,7 @@ import com.fasterxml.jackson.databind.ObjectReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -13,8 +14,10 @@ import java.util.Scanner;
 public class CsvUtils {
 
     public static String readFile(String path) {
-        try {
-            Scanner scanner = new Scanner(new File(path)).useDelimiter("\\A");
+        //try {
+            ClassLoader loader = CsvUtils.class.getClassLoader();
+            InputStream inputStream = loader.getResourceAsStream(path);
+            Scanner scanner = new Scanner(inputStream).useDelimiter("\\A");
 
             if (scanner.hasNext()) {
                 return scanner.next();
@@ -22,9 +25,9 @@ public class CsvUtils {
                 return "";
             }
 
-        } catch (FileNotFoundException e) {
+       /* } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
-        }
+        }*/
     }
 
     public static <T> List<T> readFromCsv(ObjectReader objectReader, String path) {
